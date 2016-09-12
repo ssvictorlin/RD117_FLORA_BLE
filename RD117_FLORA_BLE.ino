e1cb006da50168e10ff25ab0986470e8bd7d53e2
MaximIntegrated-RD <gordon.lee@maximintegrated.com>
2016-09-12 15:41:20 -0700

    Initial commit

---
+++ RD117_FLORA_BLE.ino
@@ -0,0 +1,314 @@
+/** \file RD117_FLORA_BLE.ino ******************************************************
+*
+* Project: RD117_FLORA_BLE#
+* Filename: RD117_FLORA_BLE.ino
+* Description: MAXREFDES117 plus BLE example program
+*
+* Revision History:
+*\n 9-8-2016 Rev 01.00 GL Initial release.
+*\n
+*
+* --------------------------------------------------------------------
+*
+* This code follows the following naming conventions:
+*
+*\n char              ch_pmod_value
+*\n char (array)      s_pmod_s_string[16]
+*\n float             f_pmod_value
+*\n int/long          n_pmod_value
+*\n int/long (array)  an_pmod_value[16]
+*\n int16_t           w_pmod_value
+*\n int16_t (array)   aw_pmod_value[16]
+*\n uint16_t          uw_pmod_value
+*\n uint16_t (array)  auw_pmod_value[16]
+*\n uint8_t           uch_pmod_value
+*\n uint8_t (array)   auch_pmod_buffer[16]
+*\n unsigned int/long un_pmod_value
+*\n int/long *             pn_pmod_value
+*
+* ------------------------------------------------------------------------- */
+/*******************************************************************************
+* Copyright (C) 2015 Maxim Integrated Products, Inc., All Rights Reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL MAXIM INTEGRATED BE LIABLE FOR ANY CLAIM, DAMAGES
+* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* Except as contained in this notice, the name of Maxim Integrated
+* Products, Inc. shall not be used except as stated in the Maxim Integrated
+* Products, Inc. Branding Policy.
+*
+* The mere transfer of this software does not imply any licenses
+* of trade secrets, proprietary technology, copyrights, patents,
+* trademarks, maskwork rights, or any other form of intellectual
+* property whatsoever. Maxim Integrated Products, Inc. retains all
+* ownership rights.
+*******************************************************************************/
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_SPI.h>
+#include <Adafruit_BluefruitLE_UART.h>
+#include <Arduino.h>
+#include <SPI.h>
+
+#include "BluefruitConfig.h"
+#include "algorithm.h"
+#include "max30102.h"
+#include "adafruit_neopixel.h"
+
+#define BRIGHTNESS_DIVISOR 8
+Adafruit_NeoPixel LED = Adafruit_NeoPixel(1, 8, NEO_GRB + NEO_KHZ800);
+
+#define MAX_BRIGHTNESS 255
+
+Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+int32_t hrmServiceId;
+int32_t hrmMeasureCharId;
+int32_t hrmLocationCharId;
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+uint32_t aun_ir_buffer[100]; //infrared LED sensor data
+uint32_t aun_red_buffer[1];  //red LED sensor data, but for this example we are not interested in SpO2, so red data can be ignored to save memory
+int32_t n_ir_buffer_length; //data length
+int32_t n_spo2;  //SPO2 value
+int8_t ch_spo2_valid;  //indicator to show if the SPO2 calculation is valid
+int32_t n_heart_rate; //heart rate value
+int8_t  ch_hr_valid;  //indicator to show if the heart rate calculation is valid
+unsigned char uch_dummy;
+unsigned long un_printed; //samples received counter
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+
+  delay(3000);
+
+  boolean success;
+
+  LED.begin();
+  LED.show();
+
+  un_printed=0;
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+  pinMode(10, INPUT);  //pin D10 connects to the interrupt output pin of the MAX30102
+  delay(1000);
+  maxim_max30102_read_reg(0,&uch_dummy);
+
+////////////////BLUETOOTH////////////////////////////////////
+  Serial.println(F("Adafruit Bluefruit Heart Rate Monitor (HRM) Example"));
+  Serial.println(F("---------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Perform a factory reset to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  Serial.println(F("IN"));
+  if (! ble.factoryReset() ){
+       error(F("Couldn't factory reset"));
+  }
+  Serial.println(F("OUT"));
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  // this line is particularly required for Flora, but is a good idea
+  // anyways for the super long lines ahead!
+  // ble.setInterCharWriteDelay(5); // 5 ms
+
+  /* Change the device name to make it easier to find */
+  Serial.println(F("Setting device name to 'Bluefruit HRM': "));
+
+  if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=Bluefruit HRM")) ) {
+    error(F("Could not set device name?"));
+  }
+
+  /* Add the Heart Rate Service definition */
+  /* Service ID should be 1 */
+  Serial.println(F("Adding the Heart Rate Service definition (UUID = 0x180D): "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x180D"), &hrmServiceId);
+  while (! success) {
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=0x180D"), &hrmServiceId);
+    //error(F("Could not add HRM service"));
+  }
+  //delay(1000);
+  /* Add the Heart Rate Measurement characteristic */
+  /* Chars ID for Measurement should be 1 */
+  Serial.println(F("Adding the Heart Rate Measurement characteristic (UUID = 0x2A37): "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A37, PROPERTIES=0x10, MIN_LEN=2, MAX_LEN=3, VALUE=00-40"), &hrmMeasureCharId);
+  while (! success) {
+      success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A37, PROPERTIES=0x10, MIN_LEN=2, MAX_LEN=3, VALUE=00-40"), &hrmMeasureCharId);
+    //error(F("Could not add HRM characteristic"));
+  }
+
+  /* Add the Body Sensor Location characteristic */
+  /* Chars ID for Body should be 2 */
+  Serial.println(F("Adding the Body Sensor Location characteristic (UUID = 0x2A38): "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A38, PROPERTIES=0x02, MIN_LEN=1, VALUE=3"), &hrmLocationCharId);
+  while (! success) {
+    success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2A38, PROPERTIES=0x02, MIN_LEN=1, VALUE=3"), &hrmLocationCharId);
+    //error(F("Could not add BSL characteristic"));
+  }
+
+  /* Add the Heart Rate Service to the advertising data (needed for Nordic apps to detect the service) */
+  Serial.print(F("Adding Heart Rate Service UUID to the advertising payload: "));
+  ble.sendCommandCheckOK( F("AT+GAPSETADVDATA=02-01-06-05-02-0d-18-0a-18") );
+
+  /* Reset the device for the new service setting changes to take effect */
+  Serial.print(F("Performing a SW reset (service changes require a reset): "));
+  ble.reset();
+
+  Serial.println();
+//////////////////////////////////////////////////////////////////////////////  
+
+  //initialize the MAX30102
+  maxim_max30102_init();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  int heart_rate = 0;
+  
+  unsigned long un_min, un_max, un_step_size, un_prev_data;
+  uint8_t uch_num_steps;//, uch_minmax_count;
+  int i;
+  unsigned long un_brightness;
+  float f_temp;
+  
+  un_brightness=0;
+  un_min=0x3FFFF;
+  un_max=0;
+  
+  n_ir_buffer_length=100;
+  //read the first 100 samples, and determine the signal range
+  for(i=0;i<n_ir_buffer_length;i++)
+  {
+    while(digitalRead(10)==1);  //wait until the interrupt pin asserts
+    maxim_max30102_read_fifo( (aun_red_buffer),(aun_ir_buffer+i));  //read from MAX30102 FIFO
+    
+    if(un_min>aun_ir_buffer[i])
+      un_min=aun_ir_buffer[i];  //update signal min
+    if(un_max<aun_ir_buffer[i])
+      un_max=aun_ir_buffer[i];  //update signal max
+    Serial.print(F("ir="));
+    Serial.print(aun_ir_buffer[i], DEC);
+  }
+  un_prev_data=aun_ir_buffer[i];
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_ir_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while(1)
+  {
+    i=0;
+    un_min=0x3FFFF;
+    un_max=0;
+
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for(i=25;i<100;i++)
+    {
+      aun_ir_buffer[i-25]=aun_ir_buffer[i];
+
+      //update the signal min and max
+      if(un_min>aun_ir_buffer[i])
+        un_min=aun_ir_buffer[i];
+      if(un_max<aun_ir_buffer[i])
+        un_max=aun_ir_buffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for(i=75;i<100;i++)
+    {
+      un_prev_data=aun_ir_buffer[i-1];
+      while(digitalRead(10)==1);
+      digitalWrite(9, !digitalRead(9));
+      maxim_max30102_read_fifo( (aun_red_buffer),(aun_ir_buffer+i));
+
+      //calculate the brightness of the LED
+      if(aun_ir_buffer[i]>un_prev_data)
+      {
+        f_temp=aun_ir_buffer[i]-un_prev_data;
+        f_temp/=(un_max-un_min);
+        f_temp*=MAX_BRIGHTNESS;
+        f_temp=un_brightness-f_temp;
+        if(f_temp<0)
+          un_brightness=0;
+        else
+          un_brightness=(int)f_temp;
+      }
+      else
+      {
+        f_temp=un_prev_data-aun_ir_buffer[i];
+        f_temp/=(un_max-un_min);
+        f_temp*=MAX_BRIGHTNESS;
+        un_brightness+=(int)f_temp;
+        if(un_brightness>MAX_BRIGHTNESS)
+          un_brightness=MAX_BRIGHTNESS;
+      }
+#if defined(ARDUINO_AVR_LILYPAD_USB)  
+      analogWrite(13, un_brightness);
+#endif
+
+#if defined(ARDUINO_AVR_FLORA8)
+      LED.setPixelColor(0, un_brightness/BRIGHTNESS_DIVISOR, 0, 0);
+      LED.show();
+#endif
+
+      //send samples and calculation result to terminal program through UART
+      Serial.print(F("ir="));
+      Serial.print(aun_ir_buffer[i], DEC);
+      
+      Serial.print(F(", HR="));
+      Serial.print(n_heart_rate, DEC);
+      
+      Serial.print(F(", HRvalid="));
+      Serial.print(ch_hr_valid, DEC);
+    }
+    maxim_heart_rate_and_oxygen_saturation(aun_ir_buffer, n_ir_buffer_length, aun_ir_buffer, &n_spo2, &ch_spo2_valid, &n_heart_rate, &ch_hr_valid); 
+    
+    ble.print( F("AT+GATTCHAR=") );
+    ble.print( hrmMeasureCharId );
+    ble.print( F(",00-") );
+    Serial.println("heartRATE");
+    if(n_heart_rate==-999)
+      n_heart_rate=0;
+    heart_rate=n_heart_rate;
+    Serial.println(heart_rate,DEC);
+    Serial.println(57,HEX);
+    ble.println(heart_rate, HEX);
+  
+    /* Check if command executed OK */
+    if ( !ble.waitForOK() )
+    {
+      Serial.println(F("Failed to get response!"));
+    }
+  }
+}
+ 
